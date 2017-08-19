9126a00a2bb3a19bd4ecebcdab5190b56f51a457
tutunak <dk@tutunak.com>
2017-08-19 20:46:02 +0300

    make migrations

--- gifach/settings.py
+++ gifach/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gif_manager.apps.GifManagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
