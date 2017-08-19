346e339fbcf0e8e27ff359f4aa08d914496e1a29
tutunak <dk@tutunak.com>
2017-08-19 20:44:47 +0300

    add simple models

--- gif_manager/models.py
+++ gif_manager/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(max_length=256, blank=False)
+    latest_post = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.tag
+
+
+class Gif(models.Model):
+    blog = models.ForeignKey(Blog)
+    tag = models.ManyToManyField(Tag)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
